e5ce42506c53c2053d7cc21144e3adfbd7882757
Geronimo Romney <g.romney@gmail.com>
2013-10-16 16:08:56 -0400

    metaprogramming: class with history

--- Practices/Cap3/metaprogramming.rb
+++ Practices/Cap3/metaprogramming.rb
@@ -7,3 +7,30 @@ class Time
 end
 
 puts Time.now.at_begining_of_year + 1.day
+
+class Class
+  def attr_accessor_with_history(name)
+      name   = name.to_s       
+      attr_reader name         
+      attr_reader name + "_history" 
+     
+    class_eval %Q{
+          def #{name}= (new_value)
+            @#{name} = new_value
+   
+            @#{name + "_history"} ||= [ nil ]
+            @#{name + "_history"} << new_value
+          end
+        }
+  end
+end
+ 
+class Foo
+  attr_accessor_with_history :bar
+end
+ 
+f = Foo.new
+f.bar = 1
+f.bar = 2
+p f.bar_history
+	
